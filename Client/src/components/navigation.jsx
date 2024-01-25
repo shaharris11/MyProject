f09db1d0ecd08a59bf80baf2e52a270f5f19a0f5
shakira <shaharris11@yahoo.com>
2024-01-25 12:05:18 -0500

    update

--- Client/src/components/navigation.jsx
+++ Client/src/components/navigation.jsx
@@ -5,6 +5,9 @@ export default function Navigation() {
     return (
         <nav id="navBar">
             <Link to='/'>Home</Link>
+            <Link to='/characters'>Characters</Link>
+            <Link to='/monsters'>Monsters</Link>
+            <Link to='/places'>Places</Link>
         </nav>
     )
 }
