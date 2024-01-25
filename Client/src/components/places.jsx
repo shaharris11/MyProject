54b28f2a93926100ac08ba7769f493f7e0885464
shakira <shaharris11@yahoo.com>
2024-01-25 12:07:32 -0500

    update

---
+++ Client/src/components/places.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+
+const apiUrl = "http://localhost:8080/api"
+
+export default function Places () {
+    const [places, setPlaces] = useState([]);
+    
+    useEffect(() => {
+        async function getPlaces() {
+            try {
+                const res = await fetch(`${apiUrl}/places`);
+                const data = await res.json();
+                setPlaces(data)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getPlaces()
+    }, [])
+    console.log(places);
+    return (
+        <>
+            <h1>Welcome to That Time I Got Reincarnated as a Slime!</h1>
+            <div className="charactersContainer">
+               {places.map((place) => {
+                return (
+                    <div key={place.id} className="place">
+                        <h2>{place.name}</h2>
+                        <h3>{place.owner}</h3>
+                        <br />
+                    </div>
+                )
+               })}
+            </div>
+        </>
+    );
+}
