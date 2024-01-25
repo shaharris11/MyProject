1624009435182510fea7c4058a7c3bf9f14a1a25
shakira <shaharris11@yahoo.com>
2024-01-25 12:04:10 -0500

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,9 @@ import {Routes, Route} from 'react-router-dom'
 import Navigation from './components/navigation'
 import Characters from './components/characters'
 import SingleCharacters from './components/singleCharacter'
+import Monsters from './components/monsters'
+import Home from './components/home'
+import Places from './components/places'
 
 
 
@@ -14,8 +17,11 @@ export default function App() {
     <>
       <Navigation />
       <Routes>
-        <Route path='/' element={<Characters />} />
-        <Route path='/characters/:characterId/details' element={<SingleCharacters /> }/>
+        <Route path='/' element={<Home />} />
+        <Route path='/characters' element={<Characters /> }/>
+        <Route path='/characters/:characterId' element={<SingleCharacters /> }/>
+        <Route path='/monsters' element={<Monsters /> }/>
+        <Route path='/places' element={<Places /> }/>
       </Routes>
     </>
   )
