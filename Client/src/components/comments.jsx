bd1f341dafaafd3cd6b3d144c673ed2343e4254b
shakira <shaharris11@yahoo.com>
2024-01-29 06:24:33 -0500

    update

--- Client/src/components/comments.jsx
+++ Client/src/components/comments.jsx
@@ -2,27 +2,27 @@ import { useState } from "react";
 import { fetchComments, makeComments, commentUpdate, deleteComment } from "./fetchingcomments";
 
 
-export default function Comments() {
-    
-    const [allComments, setAllComments] = useState()
+export default function Comments({ allComments, setAllComments }) {
+
+
     const [name, setName] = useState()
     const [description, setDescription] = useState()
-    
+
 
     const submitHandler = (event) => {
         event.preventDefault();
         async function createComment() {
-           const response = {name, description}
-           const data = await makeComments(response)
-           const results = await fetchComments(data)
-           setAllComments(allComments)
-           return results
-        } 
-    
-        async function update({characterId}) {
+            const response = { name, description }
+            const data = await makeComments(response)
+            const results = await fetchComments()
+            setAllComments(results)
+            return data
+        }
+
+        async function update({ characterId }) {
             try {
-                const response = {name, description}
-                const data = await commentUpdate(response, {characterId})
+                const response = { name, description }
+                const data = await commentUpdate(response, { characterId })
                 const results = await fetchComments()
                 setAllComments(results)
                 return data
@@ -30,9 +30,9 @@ export default function Comments() {
                 console.error(error);
             }
         }
-        async function commentdelete({characterId}) {
+        async function commentdelete({ characterId }) {
             try {
-                await deleteComment({characterId})
+                await deleteComment({ characterId })
             } catch (error) {
                 console.error(error);
             }
@@ -45,26 +45,29 @@ export default function Comments() {
     }
     return (
         <>
-        <h2>
-            Tell us what you think about this character! 
-        </h2>
+            <h2>
+                Tell us what you think about this character!
+            </h2>
             <form className="commentSection" onSubmit={submitHandler}>
                 <label>
                     Name:
-                    <input value={name}
-                            onChange={(event) => {setName(event.target.value)}}
-                     />
+                    <input
+                        placeholder="name"
+                        value={name}
+                        onChange={(event) => { setName(event.target.value) }}
+                    />
                 </label>
                 <br />
                 <label>
                     Comment:
-                    <input 
+                    <input
+                        placeholder="type comment here"
                         value={description}
-                        onChange={(event) => {setDescription(event.target.value)}}
+                        onChange={(event) => { setDescription(event.target.value) }}
                     />
                 </label>
                 <br />
-                <button type="submit">Submit</button>
+                <button type="submit">Submit Comment</button>
             </form>
         </>
     )
