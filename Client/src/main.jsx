225e91c5534a89fcf67bf6bc2d04017831cc80bf
shakira <shaharris11@yahoo.com>
2024-01-29 10:33:02 -0500

    update

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './api/store.js'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
 )
